a65e893a8bf275cd68cfb3fe55a0a86934fc2121
Venkat Jakkepalli <Venkat Jakkepalli@LAPTOP-KULC1DCB>
2022-09-08 15:54:42 +0530

    Added print statement
    Signed-off-by: Venkat Jakkepalli <Venkat Jakkepalli@LAPTOP-KULC1DCB>

--- src/javalearning/selenium.java
+++ src/javalearning/selenium.java
@@ -33,7 +33,7 @@ driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 //String print=driver.findElement(By.className(".entry-title")).getText();
 //System.out.println(print);
 System.out.println(driver.getTitle());
-System.out.println("Training completed successfully");
+System.out.println("Training completed2 successfully");
 String currentURL = driver.getCurrentUrl();
 //String currentPageSource = driver.getPageSource();
 System.out.println(currentURL);
